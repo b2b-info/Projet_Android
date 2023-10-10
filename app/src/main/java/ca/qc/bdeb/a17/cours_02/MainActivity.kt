8eb29375cbf011803e3904fe4e0994e782206c76
T3rminator2297 <francis2297@hotmail.com>
2023-10-10 11:44:46 -0400

    projet app

---
+++ app/src/main/java/ca/qc/bdeb/a17/cours_02/MainActivity.kt
@@ -0,0 +1,23 @@
+package ca.qc.bdeb.a17.cours_02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextClock
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bouton = findViewById<Button>(R.id.monBouton)
+
+        bouton.text ="toto"
+        bouton.setOnClickListener{ cliquerBouton()}
+    }
+
+    private fun cliquerBouton() {
+        findViewById<TextView>(R.id.textView).text = "toto"
+    }
+}
